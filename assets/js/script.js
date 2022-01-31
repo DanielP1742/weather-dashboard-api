0342bd327a4fc6c7df39c0fc9e2531f8daff5ca3
Daniel Putman <danielputman@gmail.com>
2022-01-30 22:31:06 -0600

    HTML page built

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+//Pseudo Coding this project out:
+
+// 3 things that need to happen:
+
+// City Searches cause city specific information populate Search results area
+
+// City Searches cause 5 day forcast to populate
+
+// City searches are saved in local storage and append divs 
